a2aeb62fc8da728b990329b0b6e52b9a81077c8c
DeniseBarbosa <deniseluvoir@gmail.com>
2022-12-27 17:00:24 -0300

    Criando o Models

---
+++ Models/Usuario.cs
@@ -0,0 +1,14 @@
+namespace Ef.Models{
+
+    //Mapeamento
+    public class Usuario{
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string  Email { get; set; }
+        public string  SenhaHash { get; set; }
+        public string Bio { get; set; }
+        public string  Image { get; set; }
+        public string Slug { get; set; }
+    }
+}
